5cb8f98e871bc5ae7b4dd67a9fcb982c12e7d0f6
Malek Smida <msmida96@gmail.com>
2022-09-24 14:07:54 +0100

    #Security: add CSP and security headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,70 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  img-src 'self' res.cloudinary.com cdn.sanity.io data:;
+  style-src 'self' 'unsafe-inline';
+  font-src 'self';
+  child-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  frame-ancestors 'none';
+  form-action 'none';
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: 'https://maleksmida.com',
+  },
+];
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['en'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
   },
-  productionBrowserSourceMaps: true
-}
+  productionBrowserSourceMaps: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
