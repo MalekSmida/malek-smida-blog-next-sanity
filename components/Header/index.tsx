8796d5c183c59ccdbcde6c73354ccab17587ad53
Malek Smida <msmida96@gmail.com>
2022-10-29 11:57:37 +0100

    #Refactor: nav naming

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,7 @@ function index() {
     <header className="flex w-full items-center justify-between p-4 text-lightdark-color">
       <Link href="/">
         <h1 className="cursor-pointer text-2xl font-bold transition-colors duration-200 ease-in-out hover:text-primary-color">
-          Blog
+          Home
         </h1>
       </Link>
       <Link href="/about">
