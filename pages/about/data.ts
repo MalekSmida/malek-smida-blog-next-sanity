90535efe74031758a5da38dd098c5bd381f27a20
Malek Smida <msmida96@gmail.com>
2022-10-29 16:52:21 +0100

    #Refactor: about page

---
+++ pages/about/data.ts
@@ -0,0 +1,31 @@
+import { AboutBlog } from '../../typings';
+
+const aboutList: AboutBlog[] = [
+  {
+    _id: '1',
+    title: 'Parable and short stories of wisdom',
+    description:
+      ', as they say "Tell me a fact and I will learn. Tell me the truth and I will believe. But tell me a story and it will live in my heart forever" -Native American proverb',
+  },
+  {
+    _id: '2',
+    title: 'Book summary and best of quotes',
+    description:
+      ', as Rene Descartes said: "The reading of all good books is like conversation with the finest men of past centuries."',
+  },
+  {
+    _id: '3',
+    title: ' Share my personal experiences that may help or inspire you',
+  },
+  {
+    _id: '4',
+    title: 'Ancient philosophy, wisdom and way of living',
+    description: ', which gonna change your life forever',
+  },
+  {
+    _id: '5',
+    title: 'Social psychology and human behaviour',
+  },
+];
+
+export { aboutList };
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,6 @@
 // local files
 import { Main } from '../../containers';
+import { aboutList } from './data';
 
 function index() {
   return (
@@ -25,8 +26,9 @@ function index() {
       </div>
       <article className="mx-auto max-w-3xl p-5">
         <h1>
-          My goal is to learn, grow, and every day become a slightly better
-          version of who I was yesterday.
+          I believe in love, love of being, love of everything as it is with no
+          judgement. I always consider myself a student of life. I hope to
+          continue learning and growing.
         </h1>
         <h1>
           I have named my blog Wanderer because that is how I would characterize
@@ -34,38 +36,12 @@ function index() {
         </h1>
         <h1 className="my-4 text-xl font-semibold">Wanderer blog is about:</h1>
         <ul className="list-disc space-y-2">
-          <li>
-            <span className="font-semibold">
-              Parable and short stories of wisdom
-            </span>
-            , as they say "Tell me a fact and I will learn. Tell me the truth
-            and I will believe. But tell me a story and it will live in my heart
-            forever" -Native American proverb{' '}
-          </li>
-          <li>
-            <span className="font-semibold">
-              Book summary and best of quotes
-            </span>
-            , as Rene Descartes said: "The reading of all good books is like
-            conversation with the finest men of past centuries." (the word
-            "good" is relative)
-          </li>
-          <li>
-            <span className="font-semibold">
-              Share my personal experiences that may help or inspire you
-            </span>
-          </li>
-          <li>
-            <span className="font-semibold">
-              Ancient philosophy, wisdom and way of living
-            </span>
-            , which gonna change your life forever
-          </li>
-          <li>
-            <span className="font-semibold">
-              Social psychology and human behaviour
-            </span>
-          </li>
+          {aboutList.map((item) => (
+            <li key={item._id}>
+              <span className="font-semibold">{item.title}</span>
+              {item.description}
+            </li>
+          ))}
         </ul>
       </article>
     </Main>
