9fe808ac5f79268a5855ea64d4133c42db0c662a
Malek Smida <msmida96@gmail.com>
2022-10-29 16:56:47 +0100

    #Refactor: post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,54 +1,19 @@
 // node modules
 import { GetStaticProps } from 'next/types';
 import PortableText from 'react-portable-text';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { useState } from 'react';
 
 // local files
 import { Main } from '../../containers';
 import { sanityClient, urlFor } from '../../services/sanity';
 import { Post } from '../../typings';
+import { CommentForm, CommentList } from '../../components';
 
-interface IFormInput {
-  _id: string;
-  name: string;
-  email: string;
-  comment: string;
-}
-
-interface Props {
+// typing
+interface PropsPost {
   post: Post;
 }
 
-function Post({ post }: Props) {
-  // hooks
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFormInput>();
-
-  // state
-  const [submitted, setSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  // handle form submit to create new comment
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    setLoading(true);
-    fetch('/api/createComment', {
-      method: 'POST',
-      body: JSON.stringify(data),
-    })
-      .then(() => {
-        setSubmitted(true);
-        setLoading(false);
-      })
-      .catch(() => {
-        setSubmitted(false);
-        setLoading(false);
-      });
-  };
-
+function Post({ post }: PropsPost) {
   return (
     <Main>
       <img
@@ -104,98 +69,8 @@ function Post({ post }: Props) {
         </div>
       </article>
 
-      <hr className="my-5 mx-auto w-full max-w-lg border border-primary-color" />
-
-      {submitted ? (
-        <div className="my-10 mx-auto flex w-full max-w-2xl flex-col bg-primary-color p-10 text-white">
-          <h3 className="mb-3 text-3xl font-bold">
-            Thank you for submitting your comment
-          </h3>
-          <p>Once it has been approved, it will appear below</p>
-        </div>
-      ) : (
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto mb-10 flex w-full max-w-2xl flex-col p-5"
-        >
-          <h4 className="text-3xl font-bold">Leave a comment bellow</h4>
-          <hr className="mt-2 py-4" />
-
-          <input
-            {...register('_id')}
-            type="hidden"
-            name="_id"
-            value={post._id}
-          />
-
-          <label className="mb-5 block">
-            <span className="text-gray-700">Name</span>
-            <input
-              {...register('name', { required: true })}
-              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-primary-color"
-              placeholder="John Doe"
-              type="text"
-            />
-          </label>
-          <label className="mb-5 block">
-            <span className="text-gray-700">Email</span>
-            <input
-              {...register('email', { required: true })}
-              className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-primary-color"
-              placeholder="example@gmail.com"
-              type="email"
-            />
-          </label>
-          <label className="block">
-            <span className="text-gray-700">Comment</span>
-            <textarea
-              {...register('comment', { required: true })}
-              className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-primary-color"
-              placeholder="What is on your mind?"
-              rows={8}
-            />
-          </label>
-
-          {/* handle error messages */}
-          <div className="flex flex-col p-5">
-            {errors.name && (
-              <span className="text-red-500">
-                ▪️ The Name Field is required
-              </span>
-            )}
-            {errors.email && (
-              <span className="text-red-500">
-                ▪️ The Email Field is required
-              </span>
-            )}
-            {errors.comment && (
-              <span className="text-red-500">
-                ▪️ The Comment Field is required
-              </span>
-            )}
-          </div>
-
-          <input
-            type="submit"
-            disabled={loading}
-            className="focus:shadow-outline cursor-pointer rounded bg-primary-color py-2 px-4 font-bold text-white shadow transition-colors duration-200 ease-in-out hover:bg-dark-color focus:outline-none disabled:bg-dark-color"
-          />
-        </form>
-      )}
-
-      {/* Comments section */}
-      <div className="mx-auto mb-10 flex w-full max-w-2xl flex-col space-y-2 p-10 shadow shadow-primary-color">
-        <h3 className="text-4xl">Comments</h3>
-        <hr className="pb-2" />
-        {post.comments?.map((comment) => (
-          <div key={comment._id}>
-            <p>
-              <span className="mr-2 text-primary-color">{comment.name}:</span>
-              {comment.comment}
-            </p>
-          </div>
-        ))}
-      </div>
+      <CommentForm post={post} />
+      <CommentList post={post} />
     </Main>
   );
 }
