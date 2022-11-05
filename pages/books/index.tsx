96ef71734949102399b0d893aa1431703a76a38f
Malek Smida <msmida96@gmail.com>
2022-11-05 16:12:54 +0100

    #Feat: books page coming soon

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -4,8 +4,8 @@ import { Main } from '../../containers';
 function index() {
   return (
     <Main withHeaderImg>
-      <div className="flex items-center justify-center p-32">
-        Coming soon ...
+      <div className="grid h-36 place-content-center">
+        <p className="tracking-widest text-gray-500">Coming soon ..</p>
       </div>
     </Main>
   );
