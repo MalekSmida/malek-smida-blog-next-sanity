ea72d948e21352183a06af3ef0b11511e38b5707
Malek Smida <msmida96@gmail.com>
2022-11-15 13:54:37 +0100

    #Upgrade: libs

--- services/sanity/index.js
+++ services/sanity/index.js
@@ -1,14 +1,15 @@
 // node modules
-import { createImageUrlBuilder, createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
 
 // local files
-import { config } from './config'
+import { config } from './config';
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config)
+export const sanityClient = createClient(config);
 
 /**
  * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  **/
-export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
