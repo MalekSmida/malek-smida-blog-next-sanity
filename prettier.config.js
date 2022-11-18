a8d63feabc449cb2e1a3b47df8a6237fc4651208
Malek Smida <msmida96@gmail.com>
2022-11-18 09:57:51 +0100

    #Feat: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,8 @@ module.exports = {
   arrowParens: 'always',
   singleQuote: true,
   tabWidth: 2,
-  // semi: false,
   tailwindConfig: './tailwind.config.js',
-}
+  semi: true,
+  printWidth: 100,
+  trailingComma: 'none'
+};
